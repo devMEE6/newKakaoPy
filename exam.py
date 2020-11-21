a1ad2eccbe8061662cfecf04ed283c4d31ca8691
nua_kr <59082853+Nua07@users.noreply.github.com>
2020-11-21 18:00:03 +0900

    Update exam.py

--- exam.py
+++ exam.py
@@ -3,6 +3,10 @@ from newKakaoPy.Chats.TextChat import TextChat
 
 app = kakaoPy("uid", "upw", "dname", "duuid")
 
+@app.packetReceive("ALL")
+async def packetReceive(packet):
+    print(packet)
+    
 @app.messageReceive()
 async def messageReceiver(message):
     print(message.message)
